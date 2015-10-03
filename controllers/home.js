b784af00e88ab168cfc327e9b61e63d997ef3fe2
raunaqrox <sahebjot94@gmail.com>
2015-10-04 02:28:17 +0530

    Categories now seen in home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,23 @@
+var Category = require('../models/Category.js');
 /**
  * GET /
  * Home page.
  */
 exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
+	if(req.user){
+		Category.find({}).sort({'addedOn': -1}).limit(3).exec(function(err, cats){
+			if(err){
+				return console.err(err);
+			}else{
+				res.render('home', {
+    				title: 'Home',
+    				categories: cats
+  				});		
+			}
+		});
+	}else{
+		res.render('home', {
+    		title: 'Home'
+  		});
+	}  
 };
