4c756180908d85f9303dbb79bec4ae68349b239c
raunaqrox <sahebjot94@gmail.com>
2015-10-04 02:52:12 +0530

    categories for all and bug fix and instagram removed

--- controllers/home.js
+++ controllers/home.js
@@ -4,20 +4,14 @@ var Category = require('../models/Category.js');
  * Home page.
  */
 exports.index = function(req, res) {
-	if(req.user){
-		Category.find({}).sort({'addedOn': -1}).limit(3).exec(function(err, cats){
-			if(err){
-				return console.err(err);
-			}else{
-				res.render('home', {
-    				title: 'Home',
-    				categories: cats
-  				});		
-			}
-		});
-	}else{
-		res.render('home', {
-    		title: 'Home'
-  		});
-	}  
+	Category.find({}).sort({'addedOn': -1}).limit(3).exec(function(err, cats){
+		if(err){
+			return console.err(err);
+		}else{
+			res.render('home', {
+				title: 'Home',
+				categories: cats
+				});		
+		}
+	});
 };
