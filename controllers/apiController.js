826542a1dbcb5eab3f23b6e969037529537d3e17
raunaqrox <sahebjot94@gmail.com>
2015-10-04 13:39:49 +0530

    links added

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,7 +1,8 @@
 var Category = require('../models/Category');
+
 exports.categorySearch = function (req, res, next) {
 	var categoryName = req.query.q;
-	Category.find({'title': {$regex: new RegExp('^.*'+ categoryName + '.*$', "i")}}).limit(5).exec(function(err, category){		
+	Category.find({'title': {$regex: new RegExp('^.*'+ categoryName + '.*$', "i")}}).limit(5).exec(function(err, category){	
 		res.send(category);
 	});
 }
--- controllers/category.js
+++ controllers/category.js
@@ -1,15 +1,26 @@
 var Category = require('../models/Category.js');
+var Link = require('../models/Link');
 
 exports.getCategory = function(req, res, next){
 	var category = req.params.name;
-	Category.findOne({'title':{$regex: new RegExp('^'+ category + '$', "i")}}, function(err, cat){
+	Category.findOne({'title':{$regex: new RegExp('^'+ category + '$', "i")}}, function(err, cat){		
 		if(!cat){
-			res.send('Category not found!');
+			req.flash('errors', { msg: 'Category not found!' });
+			res.redirect('/');
 		}else{
-			res.render('category', {		
-				catName:cat.title,
-				catDescription:cat.description
-			});
+			Link.find({_categoryId:cat._id}).limit(10).exec(function(err, links){
+				if(err) return console.err(err);
+				if(links){
+					res.render('category', {
+						catName: cat.title,
+						catDescription: cat.description,
+						links: links
+					});
+				}else{
+					req.flash('errors', { msg: 'No links found!' });
+					res.redirect('/');
+				}				
+			});			
 		}		
 	});	
 }
