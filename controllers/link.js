779a7dfa05048d6639e89496e6916720570351e6
raunaqrox <sahebjot94@gmail.com>
2015-11-18 07:25:21 +0530

    pagination added

--- controllers/link.js
+++ controllers/link.js
@@ -1,19 +1,30 @@
 var Link = require('../models/Link');
 
 exports.getLinks = function(req, res){
-	Link.find({}).limit(10).sort({'addedOn' : -1}).exec(function(err, links){
+	var perPage = 1;
+	var page = Math.max(parseInt(req.query.page), 1) || 1;
+	var from = page * perPage;
+	console.log(req.params.page, page, from);
+	Link.find({}).skip(from).limit(perPage).sort({'addedOn' : -1}).exec(function(err, links){
 		if(err) return console.error(err);
+		
 		if(!links){
 			req.flash('errors', { msg: 'Links not found!' });
 			res.render('links',{
 				title:'Links'
 			});
 		}else{			
-			res.render('links',
+			Link.count().exec(function(err, count){
+				if(err) return console.error(err);
+				res.render('links',
 				{
 					title:'Links',
-					links:links
+					links:links,
+					count: count,
+					page: page,
+					pages: count/perPage
 				});	
+			});
 		}
 	});
 }
